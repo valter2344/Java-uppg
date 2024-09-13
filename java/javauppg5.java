017a74561d86ee8e2f7631f4cfddcfb950b71ec1
valter2344 <valter.lonnqvist65@gmail.com>
2024-09-13 10:11:30 +0300

    Update javauppg5.java

--- java/javauppg5.java
+++ java/javauppg5.java
@@ -8,17 +8,17 @@ public class javauppg5 {
     
     // use if-else statements to determine the grade based on the score
     if (score >= 90 && score <= 100) {
-      System.out.println("Your grade is 5. Excellent work!");
+      System.out.println("Your grade is 5. good work!");
     } else if (score >= 80 && score <= 89) {
-      System.out.println("Your grade is 4. Good job!");
+      System.out.println("Your grade is 4. Good!");
     } else if (score >= 70 && score <= 79) {
-      System.out.println("Your grade is 3. You're on the right track!");
+      System.out.println("Your grade is 3. ok!");
     } else if (score >= 60 && score <= 69) {
-      System.out.println("Your grade is 2. Keep it up!");
+      System.out.println("Your grade is 2. berly ok!");
     } else if (score >= 25 && score <= 59) {
-      System.out.println("Your grade is 1. You need to improve.");
+      System.out.println("Your grade is 1. improv.");
     } else if (score >= 0 && score <= 25) {
-      System.out.println("Your grade is 0. You need to try harder.");
+      System.out.println("Your grade is 0. work harder.");
     } else {
       System.out.println("Invalid score. Please enter a score between 0 and 100.");
     }
